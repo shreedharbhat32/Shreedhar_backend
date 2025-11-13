4c64622afb3352073baa3ec6d1979d3a0b70045a
Shreedhar <shreedharnb.ec22@rvce.edu.in>
2025-11-13 19:01:04 +0530

    util based codes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB CONNECTION ERROR", err);
+})
 
 
 
