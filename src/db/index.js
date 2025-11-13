80a568c733ca751167620450466440bafe9832dc
Shreedhar <shreedharnb.ec22@rvce.edu.in>
2025-11-13 16:22:27 +0530

    After mongodb connection successful

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "/Shreedhar_backend/src/constants.js";
+
+const connectDB  = async ()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\nMongodb conneced ! ! DB host :${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log("Error connecting to database", error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,43 @@
+//require('dotenv').config();
+import dotenv from "dotenv";
+//import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",(error)=>{
+    //             console.error("Error in server setup", error);
+    //})
+
+//         app.listen(process.env.PORT || 3000,()=>{
+//             console.log(`Server is running on port ${process.env.PORT || 3000}`);
+//         });
+//     }catch(err){
+//         console.error("Failed to connect to MongoDB", err);
+//     }
+// }) 
+
